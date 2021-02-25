6421295e78eda0f3d9020770ad14f97667b67ec3
LazyChun <1299362885@qq.com>
2021-02-25 22:04:13 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App.js";
+import config from "./lazychun";
+
+
+window.config = config;
+ReactDom.render(<App />, document.getElementById("__$program_code$__"));
